46eaa74da1992774e35d43cc3737c86318bc85dc
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2018-12-04 11:13:46 +0100

    Improved the source files formatting

--- src/map.h
+++ src/map.h
@@ -1,8 +1,7 @@
 /*
  * map.h
  *
- *  Created on: Dec 12, 2016
- *      Author: mufferm
+ *  Created on: Dec 12, 2016 by mufferm
  */
 
 #ifndef MAP_H_
@@ -10,18 +9,17 @@
 
 class Map {
 public:
-	
-	struct single_landmark_s{
 
-		int id_i ; // Landmark ID
-		float x_f; // Landmark x-position in the map (global coordinates)
-		float y_f; // Landmark y-position in the map (global coordinates)
-	};
+  struct single_landmark_s {
 
-	std::vector<single_landmark_s> landmark_list ; // List of landmarks in the map
+    int id_i; // Landmark ID
+    float x_f; // Landmark x-position in the map (global coordinates)
+    float y_f; // Landmark y-position in the map (global coordinates)
+  };
+
+  std::vector<single_landmark_s> landmark_list; // List of landmarks in the map
 
 };
-
 
 
 #endif /* MAP_H_ */
